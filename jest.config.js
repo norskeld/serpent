9716dde8edff5b17ebce78950ed5dfcdb6eaf461
Vladislav Mamon <arcdeltax@gmail.com>
2020-06-16 02:09:59 +0500

    chore: convert jest config to json for consistency

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,16 @@
-module.exports = {
-  preset: 'ts-jest',
-  rootDir: __dirname,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'lcov', 'text'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testMatch: ['<rootDir>/src/**/__tests__/**/*spec.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  watchPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/dist/',
-    '<rootDir>/.git/',
-    '<rootDir>/.github/',
-    '<rootDir>/.vscode/',
-    '<rootDir>/.circleci/'
+{
+  "preset": "ts-jest",
+  "testMatch": ["<rootDir>/src/**/__tests__/**/*spec.ts"],
+  "testPathIgnorePatterns": ["<rootDir>/node_modules/"],
+  "coverageDirectory": "coverage",
+  "coverageReporters": ["html", "lcov", "text"],
+  "collectCoverageFrom": ["<rootDir>/src/**/*.ts"],
+  "watchPathIgnorePatterns": [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/.git/",
+    "<rootDir>/.vscode/",
+    "<rootDir>/.vscode/",	
+    "<rootDir>/.circleci/"
   ]
 }
